c916e48983811ad004031c923825facf758e0abe
Cristian Bontoiu <cbontoiu@gmail.com>
2022-02-13 17:52:52 +0000

    First Commit

---
+++ src/build/precache-manifest.a4bcecaa7115d987e707f2425d158fc2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80ee44793ff1cd5b276abac42b737cf3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "41da550c4fefa0143bd8",
+    "url": "./static/css/main.8da81a19.chunk.css"
+  },
+  {
+    "revision": "5d3e19085c251a726cba",
+    "url": "./static/js/2.ec9fc676.chunk.js"
+  },
+  {
+    "revision": "f9149ae0f0977793c838a92fcf8ae78b",
+    "url": "./static/js/2.ec9fc676.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41da550c4fefa0143bd8",
+    "url": "./static/js/main.9f017afc.chunk.js"
+  },
+  {
+    "revision": "f920a7a1c277aa85531e",
+    "url": "./static/js/runtime-main.8a278173.js"
+  }
+]);
